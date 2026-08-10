168e7b599cf520dce98bf0384a8c01750081d491
FraserW42 <hello@fraserwolfe.com>
2026-08-10 11:08:36 -0400

    Update content.js

--- content.js
+++ content.js
@@ -120,7 +120,7 @@ High Park, Toronto.`,
 
   {
     title: "Mud Colored Face — Deap",
-    still: "images/MCF_Thumbnail.png",
+    still: "images/MCF_thumbnail.png",
     video: "https://www.youtube.com/watch?v=x2DGjtzCG5A",
     meta: "Music Video · 2025 · 4 min · Producer",
     description: `A music video for artist Deap.`,
