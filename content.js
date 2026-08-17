8eb895138619f7d023f859838be8c523aee77e62
FraserW42 <hello@fraserwolfe.com>
2026-08-17 13:42:57 -0400

    Update content.js

--- content.js
+++ content.js
@@ -55,7 +55,7 @@ boundaries of their friendship.`,
 
     // Optional
     awards: [
-      "Toronto International Film Festival 2026– Official Selection",
+      "Toronto International Film Festival 2026 – Official Selection",
     ],
 
     // Optional
@@ -82,8 +82,8 @@ boundaries of their friendship.`,
 absurdities of human behaviour during the annual cherry blossom bloom in
 High Park, Toronto.`,
     awards: [
-      "Toronto International Film Festival 2026– Official Selection",
-      "Atlantic International Film Festival 2026– Official Selection",
+      "Toronto International Film Festival 2026 – Official Selection",
+      "Atlantic International Film Festival 2026 – Official Selection",
       "Young Director Award 2026 – Gold Winner",
     ],
     credits: [
